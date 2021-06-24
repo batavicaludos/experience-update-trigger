4849fe28ec24239fce06f2a77f49a02d6a206a5c
guidojw <guidojwdejong@hotmail.com>
2021-06-24 19:42:00 +0200

    feat: proper date and subshell handling
    And move parse_json_field python script to own file

--- check.sh
+++ check.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
-parse_field ()
+CHECK_INTERVAL=$(( 60 * 1000 ))
+DATE_FORMAT='%Y-%m-%dT%H:%M:%S.%3N%:z'
+
+parse_json_field ()
 {
-  value=$(python3 -c "import json, codecs; print(json.loads('$1'.encode('utf-8').decode('utf-8-sig'))['$2'])")
+  value=$(python3 parse_json_field.py "$1" $2)
   if [ $? -ne 0 ]; then
-    echo "Something went wrong while parsing the field"
-    exit 1
+    return 1
   fi
   echo "$value"
 }
 
-response=$(curl https://develop.roblox.com/v1/universes/253889617)
+parse_date ()
+{
+  echo $(date -d"$1" +'%s%3N')
+}
+
+current_date=$(date +"$DATE_FORMAT")
+current_date_ms=$(parse_date $current_date)
+echo $current_date
+echo $current_date_ms
+
+response=$(curl https://develop.roblox.com/v1/universes/2639068927)
 echo "$response"
 
-updated_at=$(parse_field "$response" "updated")
-echo "$updated_at"
+updated_at=$(parse_json_field "$response" 'updated') || {
+  echo 'Something went wrong while parsing the field'
+  exit
+}
+updated_at_ms=$(parse_date $updated_at)
+echo $updated_at
+echo $updated_at_ms
+echo $(( $current_date_ms - $updated_at_ms ))
