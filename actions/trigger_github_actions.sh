e9a3cdada72e865432ef4b20fd99d006206ba999
guidojw <guidojwdejong@hotmail.com>
2021-06-25 06:14:20 +0200

    docs: add reference to GitHub Docs

--- actions/trigger_github_actions.sh
+++ actions/trigger_github_actions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "$(date) - Triggering GitHub Actions..."
 
+# https://docs.github.com/en/rest/reference/actions#create-a-workflow-dispatch-event
 curl -s \
   -H 'Accept: application/vnd.github.v3+json' \
   -X POST \
